a09bec19c1777be9033f38f2e89a05bd03d435c3
Brandon Johnson <b@johnsonmn.org>
2019-06-09 18:05:08 +0000

    Add meta tags (hi matt)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -18,6 +18,19 @@ class MyDocument extends Document {
             name="viewport"
             content="width=device-width, initial-scale=1, maximum-scale=1"
           />
+          <title>overcommitters.club - reclaim some time</title>
+          <meta name="title" content="overcommitters.club - reclaim some time" />
+          <meta name="description" content={`Overcommitters Club is a monthly "event" for folks like us, who sign up for too many things, and need to reclaim that time every once in awhile. We like to think we're the only recurring event that's happy when you're a no-show.`} />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://overcommitters.club/" />
+          <meta property="og:title" content="overcommitters.club - reclaim some time" />
+          <meta property="og:description" content={`Overcommitters Club is a monthly "event" for folks like us, who sign up for too many things, and need to reclaim that time every once in awhile. We like to think we're the only recurring event that's happy when you're a no-show.`}/>
+
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta property="twitter:url" content="https://overcommitters.club/" />
+          <meta property="twitter:title" content="overcommitters.club - reclaim some time" />
+          <meta property="twitter:description" content={`Overcommitters Club is a monthly "event" for folks like us, who sign up for too many things, and need to reclaim that time every once in awhile. We like to think we're the only recurring event that's happy when you're a no-show.`} />
+          <meta property="twitter:image" content="" />
         </Head>
         <body className="bg-indigo-100">
           <Main />
