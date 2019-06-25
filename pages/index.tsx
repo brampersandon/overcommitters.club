4df56dde00f5f69b9b5933ddc964d09b466351a5
Brandon Johnson <b@johnsonmn.org>
2019-06-24 19:53:30 -0500

    Update for perpetual correctness

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {format, isAfter, getMonth, setMonth, setDate} from "date-fns";
 
 export default () => (
   <main className="h-screen w-full flex flex-col justify-between bg-indigo-100">
@@ -41,7 +42,9 @@ const Hero = () => (
         <span className="uppercase font-bold text-sm text-indigo-100 bg-pink-700 px-1 inline-block">
           Nope!
         </span>{" "}
-        Call a meeting of Overcommitters Club wherever you find yourself! If you're looking for a community to share your successes, or just commisserate with other overcommitters, join our Slack!
+        Call a meeting of Overcommitters Club wherever you find yourself! If
+        you're looking for a community to share your successes, or just
+        commisserate with other overcommitters, join our Slack!
       </p>
     </FAQ>
     <FAQ question="What sorts of things can I do with this time?">
@@ -56,12 +59,31 @@ const Hero = () => (
     </FAQ>
     <FAQ question="What is skipconf, and how does it relate to Overcommitters Club?">
       <p className="py-2">
-        <span className="uppercase font-bold text-sm text-indigo-100 bg-pink-700 px-1 inline-block">They're pals!</span>{" "}
-        They're philosophically similar: both pseudo-events aimed at giving people the time and space to focus on things that get put on the back burner. Overcommitters Club and skipconf share an <a className="underline" href="https://twitter.com/brandon_mn">organizer</a> and a Slack workspace (because who needs more Slack workspaces?)
+        <span className="uppercase font-bold text-sm text-indigo-100 bg-pink-700 px-1 inline-block">
+          They're pals!
+        </span>{" "}
+        They're philosophically similar: both pseudo-events aimed at giving
+        people the time and space to focus on things that get put on the back
+        burner. Overcommitters Club and skipconf share an{" "}
+        <a className="underline" href="https://twitter.com/brandon_mn">
+          organizer
+        </a>{" "}
+        and a Slack workspace (because who needs more Slack workspaces?)
       </p>
     </FAQ>
   </section>
 );
+
+const getDate = () => {
+  const now = new Date()
+  const thirteenthOfThisMonth = setDate(now, 13);
+  let nextEvent = thirteenthOfThisMonth;
+  if (isAfter(now, thirteenthOfThisMonth)) {
+    const thisMonth = getMonth(thirteenthOfThisMonth);
+    nextEvent = setMonth(nextEvent, thisMonth + 1);
+  }
+  return format(nextEvent, 'dddd MMMM Do, YYYY')
+}
 
 const CTA = () => (
   <Centerer>
@@ -70,14 +92,20 @@ const CTA = () => (
         Join the club
       </h3>
       <p className="text-center py-2 px-8 text-md">
-        <strong>Next:</strong> June 13, 2019 @ 6:30pm
+        <strong>Next:</strong> {getDate()} @ 6:30pm
       </p>
       <p className="py-2 px-8 text-md text-center">
         Take two hours back. Takes place wherever you are.
       </p>
       <div className="text-center">
-        <Button label={"Add to calendar"} href="/static/overcommitters-club.ics" />
-        <Button label={"Join the Slack"} href="https://join.slack.com/t/skipconf/shared_invite/enQtNjQ4MzIxMjk2ODk5LTIxZGMyN2RmMjUyNTJkZTQxYTM3ZDcyYmJiZjFkNTY4OTFmMGQ1YzY4MDdlZjlhNDY2MGFlN2FmMjBiZTgzNGE" />
+        <Button
+          label={"Add to calendar"}
+          href="/static/overcommitters-club.ics"
+        />
+        <Button
+          label={"Join the Slack"}
+          href="https://join.slack.com/t/skipconf/shared_invite/enQtNjQ4MzIxMjk2ODk5LTIxZGMyN2RmMjUyNTJkZTQxYTM3ZDcyYmJiZjFkNTY4OTFmMGQ1YzY4MDdlZjlhNDY2MGFlN2FmMjBiZTgzNGE"
+        />
       </div>
     </div>
   </Centerer>
@@ -94,7 +122,10 @@ const FAQ: React.FC<{
   </div>
 );
 
-const Button: React.FC<{ label: React.ReactNode, href: string }> = ({ label, href }) => (
+const Button: React.FC<{ label: React.ReactNode; href: string }> = ({
+  label,
+  href
+}) => (
   <a
     href={href}
     className="px-4 py-2 m-2 bg-pink-700 text-indigo-100 font-bold text-lg rounded lowercase inline-block"
@@ -104,9 +135,7 @@ const Button: React.FC<{ label: React.ReactNode, href: string }> = ({ label, hre
 );
 
 const Centerer: React.FC = ({ children }) => (
-  <div className="mx-auto justify-center content-center">
-    {children}
-  </div>
+  <div className="mx-auto justify-center content-center">{children}</div>
 );
 
 const Footer: React.FC = () => (
