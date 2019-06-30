34607a968c919931133d83773c857eb84e046a81
Matt Decuir <decuirm@gmail.com>
2019-06-30 12:32:28 -0500

    Remove events.pizza artifacts 🍕😭

--- layouts/index.tsx
+++ layouts/index.tsx
@@ -5,8 +5,8 @@ import "../styles/index.css";
 const PageLayout: React.FC = ({ children }) => (
   <div className="container h-screen content-center">
     <div className="content-center flex-col">
-      <h1 className="text-center text-6xl">events.pizza 🍕</h1>
-      <h2 className="text-center text-4xl">run events without sadness.</h2>
+      <h1 className="text-center text-6xl">Overcommitters Club</h1>
+      <h2 className="text-center text-4xl">reclaim some time</h2>
     </div>
     <div className="container content-center justify-center flex py-8">
       {children}
