18092a90b0fa51f6022c4c5c78bf5b57bd699de2
Matt Decuir <decuirm@gmail.com>
2019-06-30 12:29:14 -0500

    Update meta tags (hi brandon)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,6 +11,9 @@ class MyDocument extends Document {
   }
 
   render() {
+    const title = "Overcommitters Club - reclaim some time";
+    const description = `Overcommitters Club is a monthly "event" for folks like us, who sign up for too many things, and need to reclaim that time every once in awhile. We like to think we're the only recurring event that's happy when you're a no-show.`;
+
     return (
       <Html>
         <Head>
@@ -18,18 +21,18 @@ class MyDocument extends Document {
             name="viewport"
             content="width=device-width, initial-scale=1, maximum-scale=1"
           />
-          <title>overcommitters.club - reclaim some time</title>
-          <meta name="title" content="overcommitters.club - reclaim some time" />
-          <meta name="description" content={`Overcommitters Club is a monthly "event" for folks like us, who sign up for too many things, and need to reclaim that time every once in awhile. We like to think we're the only recurring event that's happy when you're a no-show.`} />
+          <title>{title}</title>
+          <meta name="title" content={title} />
+          <meta name="description" content={description} />
           <meta property="og:type" content="website" />
           <meta property="og:url" content="https://overcommitters.club/" />
-          <meta property="og:title" content="overcommitters.club - reclaim some time" />
-          <meta property="og:description" content={`Overcommitters Club is a monthly "event" for folks like us, who sign up for too many things, and need to reclaim that time every once in awhile. We like to think we're the only recurring event that's happy when you're a no-show.`}/>
+          <meta property="og:title" content={title} />
+          <meta property="og:description" content={description} />
 
           <meta property="twitter:card" content="summary_large_image" />
           <meta property="twitter:url" content="https://overcommitters.club/" />
-          <meta property="twitter:title" content="overcommitters.club - reclaim some time" />
-          <meta property="twitter:description" content={`Overcommitters Club is a monthly "event" for folks like us, who sign up for too many things, and need to reclaim that time every once in awhile. We like to think we're the only recurring event that's happy when you're a no-show.`} />
+          <meta property="twitter:title" content={title} />
+          <meta property="twitter:description" content={description} />
           <meta property="twitter:image" content="" />
         </Head>
         <body className="bg-indigo-100">
